23aee53886adec28c7faf05431e3d153e4132d2f
Brighton Sibanda <brightonsibanda2025@u.northwestern.edu>
2023-02-21 21:26:19 -0600

    home

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,34 @@
 import React from 'react'
-import {StyleSheet, Text, View} from 'react-native'
+import {StyleSheet, Text, View, Button} from 'react-native'
 
 const HomeScreen = () => {
     return (
-        <View>
-            <Text>Home Screen</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>Home Screen</Text>
+            <View style={styles.buttonContainer}>
+                <Button title="Button 1" />
+                <Button title="Button 2" />
+            </View>
         </View>
     )
 }
 
 export default HomeScreen
-const styles = StyleSheet.create({})
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 24,
+        marginBottom: 20,
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '80%',
+    },
+})
